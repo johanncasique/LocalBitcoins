afa279b6f8615b5e3d185a804f434b8e145082e4
Unknown <casiquejohann@gmail.com>
2019-01-24 22:10:38 +0100

    create xcode proyect

---
+++ LocalBitcoin/MainList/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LocalBitcoin
+//
+//  Created by johann on 24/01/2019.
+//  Copyright © 2019 johann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
