a9e95e2e24d40421ddec33a26246198919401ab3
Johann Casique <johan.casique@gft.com>
2019-03-12 11:55:54 +0100

    fix timeStamp bug, now call to service is working

---
+++ LocalBitcoin/Shared/Utilities/Extensions/DataExtension.swift
@@ -0,0 +1,21 @@
+//
+//  DataExtension.swift
+//  LocalBitcoin
+//
+//  Created by Casique, Johan on 11/03/2019.
+//  Copyright © 2019 johann. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+    
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+        return map { String(format: format, $0) }.joined()
+    }
+}
