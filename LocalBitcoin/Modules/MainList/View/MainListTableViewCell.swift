d00d580fe850f59afca1993c8cd68e1062b45b66
Johann <casiquejohann@gmail.com>
2019-06-29 23:30:05 +0200

    WIP

--- LocalBitcoin/Modules/MainList/View/MainListTableViewCell.swift
+++ LocalBitcoin/Modules/MainList/View/MainListTableViewCell.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class MainListTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var shadowView: UIView!
+    @IBOutlet weak var firstStackContentView: UIView!
+    @IBOutlet weak var lastStackContentView: UIView!
     @IBOutlet weak var sellerLabel: UILabel!
-    @IBOutlet weak var paymentMethodLabel: UILabel!
-    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var ratingImage: UIImageView!
+    @IBOutlet weak var minAmountLimitsLabel: UILabel!
     @IBOutlet weak var limitsLabel: UILabel!
+    @IBOutlet weak var maxAmountLimitsLabel: UILabel!
+    @IBOutlet weak var separatorLimitsView: UIView!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var iconTypeTransactionImage: UIImageView!
+    @IBOutlet weak var typeTransactionLabel: UILabel!
+    @IBOutlet weak var buyIconImageView: UIImageView!
     
-    func config(with viewModel: MainListViewModel) {
-        sellerLabel.text = viewModel.getSellerName
-        paymentMethodLabel.text = viewModel.getPaymentsMethod
-        priceLabel.text = viewModel.getPrice
-        limitsLabel.text = viewModel.getLimits
+    override func draw(_ rect: CGRect) {
+        firstStackContentView.roundCorners(corners: [.topLeft, .topRight], radius: 5)
+        lastStackContentView.roundCorners(corners: [.bottomLeft, .bottomRight], radius: 5)
+        
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOpacity = 1
+        shadowView.layer.shadowOffset = .zero
+        shadowView.layer.shadowRadius = 10
     }
     
+    func config(with viewModel: MainListViewModel) {
+//        sellerLabel.text = viewModel.getSellerName
+//        priceLabel.text = viewModel.getPrice
+//        limitsLabel.text = viewModel.getLimits
+        sellerLabel.text = viewModel.getSellerName
+        ratingLabel.text = "(1000 + 99%)"
+        ratingImage.image = UIImage(named: "goodRating")
+        minAmountLimitsLabel.text = "1,000"
+        limitsLabel.text = "Limits"
+        maxAmountLimitsLabel.text = "2,000"
+        priceLabel.text = viewModel.getPrice
+        iconTypeTransactionImage.image = UIImage(named: "transfer")
+        typeTransactionLabel.text = "SEPA (EU) bank transfer"
+        buyIconImageView.image = UIImage(named: "ic_next")
+    }
+    
+}
+
+extension UIView {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
 }
